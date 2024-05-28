10e42ccaefbce114e8189b2b5c94e4def4b21591
Murali D <muralindiablog@gmail.com>
2024-05-27 21:00:51 -0700

    action js

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,16 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  
+// 1. Get 3 inputs
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  //2. upload to s3
+  const s3Uri = `s3://${bucket}`
+exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
   core.notice('Hello from my custom javascript action!')
 }
 
